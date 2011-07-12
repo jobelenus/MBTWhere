689d9081f5e597f38df3c01a39b239dc60b90913
John Obelenus <jobelenus@gmail.com>
2011-07-12 16:16:27 -0400

    sort times

--- src/com/af/mbtwhere/RouteLayout.java
+++ src/com/af/mbtwhere/RouteLayout.java
@@ -1,6 +1,14 @@
 package com.af.mbtwhere;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 import android.content.Context;
 import android.util.Log;
@@ -39,7 +47,27 @@ public class RouteLayout extends LinearLayout {
 	}
 	
 	public void setDisplay(ArrayList<String> times) {
+		TreeMap<Integer, String> sortedTimes = new TreeMap<Integer, String>();
 		for(String time : times) {
+			try {
+				List<String> sArr = (List<String>) Arrays.asList(time.split(":"));
+				Collections.reverse(sArr);
+				Iterator it = sArr.iterator();
+				int seconds = 0;
+				int i = 0;
+				while(it.hasNext()) {
+					seconds += Math.max((60*i++),1) * new Integer((String) it.next());
+				}
+				sortedTimes.put(seconds, time);
+			} catch(NumberFormatException e) { //we got a string message, leave it be, put it at the end.
+				sortedTimes.put(999999, time);
+			}
+		}
+		Set set = sortedTimes.entrySet();
+		Iterator i = set.iterator();
+		while(i.hasNext()) {
+			Map.Entry aTime = (Map.Entry)i.next();
+			String time = (String) aTime.getValue();
 			Log.v(TAG, "time: "+time);
 			LayoutInflater inflater = (LayoutInflater)c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 			inflater.inflate(R.layout.time_text, getTimes());
